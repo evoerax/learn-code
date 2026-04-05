a4b6b16a9fe41c96979dfffdafc730838457c4e8
evoerax <thenovoni@gmail.com>
2026-04-05 21:22:25 +0800

    fttm #2: Created bilingual pages for Autocode (1952) and FLOW-MATIC (1955), adding 4 new pages to the 1950s timeline section with build verification passing (124 pages indexed).

--- web/content/en/timeline/1950s/_meta.js
+++ web/content/en/timeline/1950s/_meta.js
@@ -1,6 +1,8 @@
 export default {
   index: { title: 'Overview' },
+  autocode: { title: 'Autocode' },
   fortran: { title: 'FORTRAN' },
+  'flow-matic': { title: 'FLOW-MATIC' },
   lisp: { title: 'LISP' },
   cobol: { title: 'COBOL' },
   assembly: { title: 'Assembly' },
--- web/content/zh/timeline/1950s/_meta.js
+++ web/content/zh/timeline/1950s/_meta.js
@@ -1,6 +1,8 @@
 export default {
   index: { title: '概览' },
+  autocode: { title: 'Autocode' },
   fortran: { title: 'FORTRAN' },
+  'flow-matic': { title: 'FLOW-MATIC' },
   lisp: { title: 'LISP' },
   cobol: { title: 'COBOL' },
   assembly: { title: '汇编' },
