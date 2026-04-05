489064b5c930ddd52c1a5ea1d1fe5baccc4d0f2b
evoerax <thenovoni@gmail.com>
2026-04-05 10:07:53 +0800

    fttm #7: Created bilingual pages for 5 programming languages from the 2000s: C#, Scala, F#, Go, and Rust, with properly updated _meta.js files for the 2000s decade directory.

---
+++ web/content/en/timeline/2000s/_meta.js
@@ -0,0 +1,8 @@
+export default {
+  index: { title: 'Overview' },
+  'c-sharp': { title: 'C#' },
+  scala: { title: 'Scala' },
+  'f-sharp': { title: 'F#' },
+  go: { title: 'Go' },
+  rust: { title: 'Rust' },
+}
---
+++ web/content/zh/timeline/2000s/_meta.js
@@ -0,0 +1,8 @@
+export default {
+  index: { title: '概览' },
+  'c-sharp': { title: 'C#' },
+  scala: { title: 'Scala' },
+  'f-sharp': { title: 'F#' },
+  go: { title: 'Go' },
+  rust: { title: 'Rust' },
+}
