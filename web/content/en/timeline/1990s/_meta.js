d9544b2cdf154468e60c911eb46cfe8bda2d618b
evoerax <thenovoni@gmail.com>
2026-04-05 22:12:51 +0800

    fttm #12: Created 3 bilingual programming language pages (Visual Basic 1991, Delphi 1995, OCaml 1996) for the 1990s timeline section, updated _meta.js files with correct chronological ordering, build verified with 172 pages indexed.

--- web/content/en/timeline/1990s/_meta.js
+++ web/content/en/timeline/1990s/_meta.js
@@ -1,11 +1,14 @@
 export default {
   index: { title: 'Overview' },
+  haskell: { title: 'Haskell' },
   python: { title: 'Python' },
+  'visual-basic': { title: 'Visual Basic' },
+  lua: { title: 'Lua' },
+  r: { title: 'R' },
+  delphi: { title: 'Delphi' },
   java: { title: 'Java' },
   javascript: { title: 'JavaScript' },
   php: { title: 'PHP' },
   ruby: { title: 'Ruby' },
-  haskell: { title: 'Haskell' },
-  lua: { title: 'Lua' },
-  r: { title: 'R' },
+  ocaml: { title: 'OCaml' },
 }
