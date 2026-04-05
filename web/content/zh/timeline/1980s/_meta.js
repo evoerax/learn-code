6aa0c61110ff63594991430f3c5ed77943490d5e
evoerax <thenovoni@gmail.com>
2026-04-05 21:52:09 +0800

    fttm #10: Created 3 bilingual pages for 1980s programming languages (Common Lisp, Eiffel, ABAP), updated _meta.js with correct chronological ordering, build verified with 158 pages indexed.

--- web/content/zh/timeline/1980s/_meta.js
+++ web/content/zh/timeline/1980s/_meta.js
@@ -1,10 +1,13 @@
 export default {
   index: { title: '概览' },
-  'c++': { title: 'C++' },
   ada: { title: 'Ada' },
+  'c++': { title: 'C++' },
+  'objective-c': { title: 'Objective-C' },
+  abap: { title: 'ABAP' },
+  'common-lisp': { title: 'Common Lisp' },
   matlab: { title: 'MATLAB' },
+  eiffel: { title: 'Eiffel' },
+  erlang: { title: 'Erlang' },
   perl: { title: 'Perl' },
   vhdl: { title: 'VHDL' },
-  'objective-c': { title: 'Objective-C' },
-  erlang: { title: 'Erlang' },
 }
