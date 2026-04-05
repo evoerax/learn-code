b441ea4726523a7e9fcf62c8d016a9065aaa1351
evoerax <thenovoni@gmail.com>
2026-04-05 09:53:03 +0800

    fttm #3: Created bilingual pages for 3 programming languages from the 1960s: ALGOL, PL/I, and RPG, with properly updated _meta.js files for the 1960s decade directory.

---
+++ web/content/en/timeline/1960s/_meta.js
@@ -0,0 +1,6 @@
+export default {
+  index: { title: 'Overview' },
+  algol: { title: 'ALGOL' },
+  'pl-i': { title: 'PL/I' },
+  rpg: { title: 'RPG' },
+}
---
+++ web/content/zh/timeline/1960s/_meta.js
@@ -0,0 +1,6 @@
+export default {
+  index: { title: '概览' },
+  algol: { title: 'ALGOL' },
+  'pl-i': { title: 'PL/I' },
+  rpg: { title: 'RPG' },
+}
