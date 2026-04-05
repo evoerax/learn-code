91f2179c327fbc366ea7046f2339b6a37fa163c3
evoerax <thenovoni@gmail.com>
2026-04-05 09:59:25 +0800

    fttm #5: Created bilingual pages for 5 programming languages from the 1980s: C++, Ada, MATLAB, Perl, and VHDL, with properly updated _meta.js files for the 1980s decade directory.

---
+++ web/content/zh/timeline/1980s/_meta.js
@@ -0,0 +1,8 @@
+export default {
+  index: { title: '概览' },
+  'c++': { title: 'C++' },
+  ada: { title: 'Ada' },
+  matlab: { title: 'MATLAB' },
+  perl: { title: 'Perl' },
+  vhdl: { title: 'VHDL' },
+}
