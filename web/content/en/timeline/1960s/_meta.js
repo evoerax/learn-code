c56ae46247da2625b703922dd3509b88748eb269
evoerax <thenovoni@gmail.com>
2026-04-05 21:29:06 +0800

    fttm #4: Created bilingual pages for 3 more 1960s programming languages (SNOBOL, APL, BASIC), updated _meta.js with correct chronological ordering, build passing with 136 pages indexed.

--- web/content/en/timeline/1960s/_meta.js
+++ web/content/en/timeline/1960s/_meta.js
@@ -1,7 +1,10 @@
 export default {
   index: { title: 'Overview' },
   algol: { title: 'ALGOL' },
+  snobol: { title: 'SNOBOL' },
   'pl-i': { title: 'PL/I' },
   rpg: { title: 'RPG' },
+  basic: { title: 'BASIC' },
+  apl: { title: 'APL' },
   simula: { title: 'Simula' },
 }
--- web/content/zh/timeline/1960s/_meta.js
+++ web/content/zh/timeline/1960s/_meta.js
@@ -1,7 +1,10 @@
 export default {
   index: { title: '概览' },
   algol: { title: 'ALGOL' },
+  snobol: { title: 'SNOBOL' },
   'pl-i': { title: 'PL/I' },
   rpg: { title: 'RPG' },
+  basic: { title: 'BASIC' },
+  apl: { title: 'APL' },
   simula: { title: 'Simula' },
 }
